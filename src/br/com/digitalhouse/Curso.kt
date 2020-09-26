a39786ebdcb8a83b5db61360645c58f0e66ced08
maraisi <maraisidepaula@gmail.com>
2020-09-25 21:55:18 -0300

    parte b: curso

---
+++ src/br/com/digitalhouse/Curso.kt
@@ -0,0 +1,10 @@
+package br.com.digitalhouse
+
+//Parte B: criar um diagrama de classes que modele a classe Curso. A princípio, um curso possui um nome (String)
+// e um código de curso (Integer). Implementar a classe criando os atributos necessários.
+
+//Um curso será igual a outro se seus códigos de curso forem iguais.
+
+class Curso (val nome:String, val codigoCurso: Int){
+
+}
