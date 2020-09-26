628f5d77de97c64b476a6b90bbcf1a251c51f205
maraisi <maraisidepaula@gmail.com>
2020-09-25 22:21:28 -0300

    parte f: matricula

---
+++ src/br/com/digitalhouse/Matricula.kt
@@ -0,0 +1,9 @@
+package br.com.digitalhouse
+
+//Criar um diagrama de classes que modele a classe Matrícula. A princípio, uma matrícula tem um aluno (Aluno),
+//um curso (Curso) e uma data de matrícula (Date). Implementar a classe criando os atributos necessários.
+//Criar um construtor de Matrícula que tome um aluno e um curso e construa uma matrícula com a data do dia.
+
+class Matricula(var aluno: Aluno, var curso: Curso, val dataDeMatricula: Date ) {
+
+}
