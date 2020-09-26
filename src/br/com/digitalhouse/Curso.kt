50668d278f1dbfd95a39296e4385544fbed1307e
maraisi <maraisidepaula@gmail.com>
2020-09-25 22:11:41 -0300

    parte e: curso

--- src/br/com/digitalhouse/Curso.kt
+++ src/br/com/digitalhouse/Curso.kt
@@ -2,9 +2,21 @@ package br.com.digitalhouse
 
 //Parte B: criar um diagrama de classes que modele a classe Curso. A princípio, um curso possui um nome (String)
 // e um código de curso (Integer). Implementar a classe criando os atributos necessários.
-
 //Um curso será igual a outro se seus códigos de curso forem iguais.
 
-class Curso (val nome:String, val codigoCurso: Int){
+//Parte E: além de ter um nome e código de curso, um curso possui um professor titular (ProfessorTitular),
+//um professor adjunto (ProfessorAdjunto), uma quantidade máxima de alunos (Integer) e uma lista de alunos
+//matriculados.
+
+
+class Curso (
+    val nome:String,
+    val codigoCurso: Int,
+    var professorTitular: ProfessorTitular,
+    var professorAdjunto: ProfessorAdjunto,
+    var qtdMaxDeAlunos: Int,
+    val listaAlunosMatriculados: mutableListOf
+) {
+
 
 }
